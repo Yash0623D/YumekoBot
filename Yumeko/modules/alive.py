989b0586fc363079b1f61d611f7f61eabb20f538
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-01-14 02:12:18 +0530

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -7,14 +7,14 @@ from pyrogram import __version__ as pyrover
 from Yumeko.events import register as MEMEK
 from Yumeko import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
+PHOTO = "https://telegra.ph/file/215dda7cc7cc720273495.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
   YUMEKO = "**Holla I'm Yumeko!** \n\n"
   YUMEKO += "×**I'm Working Properly** \n\n"
-  YUMEKO += "×**My Owners : [Tarun • [ᴀc͜͡ɢᴄ]](https://t.me/TheBlacklinen), [亗 ʀʏᴜ](https://t.me/Ryu_God)** \n\n"
+  YUMEKO += "×**My Owners : [ʀᴏʀᴏɴᴏᴀ|ᴢᴏʀᴏ]](https://t.me/Aceladi), [Asᴛᴀ](https://t.me/baby_hoi)** \n\n"
   YUMEKO += f"×**Telethon Version : {tlhver}** \n\n"
   YUMEKO += f"×**Pyrogram Version : {pyrover}** \n\n"
   YUMEKO += "**Thanks For Adding Me Here ❤️**"
